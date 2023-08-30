a86dabb91e7e082e37e5896b7fb3e6e824ab0ab6
Massimo Costa <costa.massimo@gmail.com>
2023-08-30 12:58:36 +0200

    feat: add template with log/slog logger

---
+++ with-logger/README.md
@@ -0,0 +1,21 @@
+# Project-Name  <!-- # put your here -->
+
+<!-- put some description here -->
+
+## Usage
+
+<!-- instructions about how to use -->
+
+### Command line flags
+
+| Flag          | Environment Variable | Description                                              | Default Value |
+| ------------- | -------------------- | -------------------------------------------------------- | ------------- |
+| `-log.level`  | `APP_LOG_LEVEL`      | Application minimum log level (DEBUG, INFO, WARN, ERROR) | "INFO"        |
+| `-log.format` | `APP_LOG_FORMAT`     | Application log level format (text, json)                | "text"        |
+| `-v`          |                      | Print version and exit                                   |               |
+| `-V`          |                      | Print build information and exit                         |               |
+| `-h`          |                      | Print help and exit                                      |               |
+
+## Development
+
+<!-- instructions about how to contribute -->
---
+++ with-logger/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"log/slog"
+)
+
+const AppName = "go-template"
+
+// build info
+var (
+	version = "development"
+	commit  = "N.A."
+	date    = "N.A."
+)
+
+// flags
+var (
+	fs *flag.FlagSet
+
+	showVersion   bool
+	showBuildInfo bool
+
+	logLevel  string
+	logFormat string
+)
+
+func init() {
+	fs = flag.NewFlagSet(AppName, flag.ExitOnError)
+
+	fs.BoolVar(&showVersion, "v", false, "Print version and exit")
+	fs.BoolVar(&showBuildInfo, "V", false, "Print build information and exit")
+
+	fs.StringVar(&logFormat, "log.format", getEnv("APP_LOG_FORMAT", "text"), "Log format (text, json)")
+	fs.StringVar(&logLevel, "log.level", getEnv("APP_LOG_LEVEL", slog.LevelInfo.String()), "Log level (debug, info, warn, error)")
+}
+
+func main() {
+	fs.Parse(os.Args[1:])
+
+	if showVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
+	if showBuildInfo {
+		fmt.Printf("Version:%s, GitCommit:%s, BuildDate:%s\n", version, commit, date)
+		os.Exit(0)
+	}
+
+	logger := initLogger()
+
+	logger.Info("started")
+	defer logger.Info("stopped")
+}
+
+func getEnv(key string, defaultValue string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return defaultValue
+}
+
+func initLogger() *slog.Logger {
+	var lvl slog.Level
+	if err := lvl.UnmarshalText([]byte(logLevel)); err != nil {
+		lvl = slog.LevelInfo
+	}
+	opts := &slog.HandlerOptions{
+		Level:     lvl,
+		AddSource: true,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			switch a.Key {
+			case slog.TimeKey:
+				// use UTC time
+				a.Value = slog.TimeValue(a.Value.Time().UTC())
+
+			case slog.SourceKey:
+				// remove directories from File
+				source := a.Value.Any().(*slog.Source)
+				source.File = filepath.Base(source.File)
+			}
+			return a
+		},
+	}
+
+	var h slog.Handler = slog.NewTextHandler(os.Stdout, opts)
+	if logFormat == "json" {
+		h = slog.NewJSONHandler(os.Stdout, opts)
+	}
+	return slog.New(h)
+}
